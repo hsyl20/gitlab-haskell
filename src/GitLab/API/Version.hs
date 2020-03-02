b14e0fbe3fcfe4c8a2734fa80ab441be6c0f461d
Rob Stewart <robstewart57@gmail.com>
2020-03-02 22:24:14 +0000

    A Safer API: introduction of Either types

---
+++ src/GitLab/API/Version.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Module      : Version
+-- Description : Queries about GitLab instance version
+-- Copyright   : (c) Rob Stewart, Heriot-Watt University, 2020
+-- License     : BSD3
+-- Maintainer  : robstewart57@gmail.com
+-- Stability   : stable
+module GitLab.API.Version where
+
+import Control.Monad.IO.Class
+import GitLab.Types
+import GitLab.WebRequests.GitLabWebCalls
+import Network.HTTP.Types.Status
+
+gitlabVersion :: (MonadIO m) => GitLab m (Either Status (Maybe Version))
+gitlabVersion = do
+  let path = "/version"
+  gitlabOne path
